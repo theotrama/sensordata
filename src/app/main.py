4ef9fbfbc5e89a3ce83e8b0fa7ac0bff0741aedf
theotrama <jankoch15@yahoo.de>
2020-09-16 22:53:57 +0200

    Small change in main

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import sensor
+from .database import SessionLocal
 
 app = FastAPI()
+
+
+async def get_token_header(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
 app.add_middleware(
@@ -20,4 +26,5 @@ app.include_router(
     sensor.router,
     prefix="/sensors",
     tags=["sensors"],
+    dependencies=[Depends(get_token_header)],
 )
