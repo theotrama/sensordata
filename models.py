53fea447af264d02c3eba774013741cbff410d66
theotrama <jankoch15@yahoo.de>
2020-09-16 11:54:55 +0200

    Integrate alembic for simple db migrations

--- models.py
+++ models.py
@@ -1,18 +1,34 @@
 import datetime
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
 
 Base = declarative_base()
 
 
 class Sensor(Base):
-    __tablename__ = 'sensors'
+
+    __tablename__ = "sensors"
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
     type = Column(String)
     unit = Column(String)
     date_added = Column(DateTime, default=datetime.datetime.utcnow)
+    test = Column(String)
 
     def __repr__(self):
-        return f"<Book(name='{self.name}', type='{self.type}', date_added={self.date_added})>"
+        return f"<Sensor(name='{self.name}', type='{self.type}', date_added={self.date_added})>"
+
+
+class Measurement(Base):
+
+    __tablename__ = "measurements"
+
+    id = Column(Integer, primary_key=True)
+    sensor_id = Column(Integer, ForeignKey('sensors.id', ondelete='CASCADE'))
+    data = Column(Float)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Measurement(data='{self.data}', timestamp='{self.timestamp}', sensor_id={self.sensor_id})>"
