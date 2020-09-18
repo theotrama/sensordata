4578dc1f1deee7fc70b53bb617c614ba035604c6
theotrama <jankoch15@yahoo.de>
2020-09-18 11:14:41 +0200

    Restructure project for Vue.js integration

--- backend/src/alembic/env.py
+++ backend/src/alembic/env.py
@@ -8,8 +8,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import Config
-from api.models import Base
+from app.config import Config
+from app.api.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
